dc0863d034f85b32eee6e5f87fff98217640538b
suryansh2121 <rajputsuryansh491@gmail.com>
2024-09-15 22:25:50 +0530

    all case sentivity is removed for vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Footer from "@/components/footer/footer";
+import Footer from "@/components/footer/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import AuthProvider from "@/providers/AuthProvider";
 
